05e2639693e81a70f263aa7f30b78a6be79932a0
Sam Desborough <sam.desborough@gmail.com>
2016-06-10 11:17:31 +0100

    more

---
+++ static/js/components/name.jsx
@@ -0,0 +1,38 @@
+import socket from '../socket';
+
+const NameEntry = React.createClass({
+  getInitialState() {
+      return {
+          name: ''
+      };
+  },
+
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  },
+
+  submit() {
+    this.props.onSubmit(this.state.name);
+  },
+
+  render() {
+    return (
+      <div>
+        <section className="name-entry">
+          <input value={this.state.name} onChange={this.updateName}
+                  placeholder="enter your name" />
+        </section>
+
+        <section className="submit-button">
+          <div>
+            <button onClick={this.submit}>PLAY</button>
+          </div>
+        </section>
+      </div>
+    );
+  }
+});
+
+export default NameEntry;
---
+++ static/js/socket.js
@@ -0,0 +1,3 @@
+const socket = io.connect(`${window.location.hostname}:3000`);
+
+export default socket;
