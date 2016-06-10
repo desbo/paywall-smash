a136f08a4b947cd2ef33e209331eabd038e2fe91
Sam Desborough <sam.desborough@gmail.com>
2016-06-10 14:34:11 +0100

    fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('static'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', { env: NODE_ENV });
+  res.render('index', { env: process.env.NODE_ENV });
 });
 
 app.get('/controls', (req, res) => {
