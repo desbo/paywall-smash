6266ef780bb665cc5bfd33915828e13cdf9ca770
Sam Desborough <sam.desborough@gmail.com>
2016-06-10 14:41:20 +0100

    fucking fix

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,3 +1,3 @@
-const socket = io.connect(`${window.location.hostname}:3000`);
+const socket = io.connect(window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '');
 
 export default socket;
